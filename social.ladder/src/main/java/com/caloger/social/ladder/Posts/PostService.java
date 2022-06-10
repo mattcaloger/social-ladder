698dbdaba6ec0d835d4ea2eb9bbb01c52128e639
mcaloger <mattcaloger@gmail.com>
2022-06-09 22:22:19 -0400

    Update PostService.java

--- social.ladder/src/main/java/com/caloger/social/ladder/Posts/PostService.java
+++ social.ladder/src/main/java/com/caloger/social/ladder/Posts/PostService.java
@@ -1,7 +1,19 @@
 package com.caloger.social.ladder.Posts;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostService {
+
+    private PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public PostModel createPost(PostModel post) {
+        return this.postRepository.save(post);
+    }
 }
