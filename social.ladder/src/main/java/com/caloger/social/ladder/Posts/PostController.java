039c0888d83ab3af3737b20743a9fa84a11b3a74
mcaloger <mattcaloger@gmail.com>
2022-06-11 20:45:40 -0400

    Update PostController.java

--- social.ladder/src/main/java/com/caloger/social/ladder/Posts/PostController.java
+++ social.ladder/src/main/java/com/caloger/social/ladder/Posts/PostController.java
@@ -1,8 +1,24 @@
 package com.caloger.social.ladder.Posts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @RequestMapping("/post/")
 public class PostController {
+
+    private PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/")
+    public Optional<PostModel> getPost(Long Id) {
+        return postService.readPost(Id);
+    }
 
 }
